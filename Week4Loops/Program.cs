f43b8bf382e723d99204ecaaea9946b322080b6f
K4spar <kaspar.salvet@tktk.ee>
2023-02-08 13:42:07 +0200

    Add week4 code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,8 @@
+﻿
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine("We don't need no education.");
+}
+
+Console.WriteLine("Have a nice day!");
+//Console.WriteLine("We don't need no education.");
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,23 @@
+﻿Random rnd= new Random();
+int cpuRandom;
+
+bool loopActive = true;
+
+while(loopActive)
+{
+    cpuRandom = rnd.Next(1, 4);
+    //Console.WriteLine($"cpu has generated: {cpuRandom}");
+    Console.WriteLine("Make a guess. Enter a number 1-3");
+    int userNum = Int32.Parse(Console.ReadLine());  
+
+    if(userNum == cpuRandom)
+    {
+        Console.WriteLine("Congratulations, you won!");
+        loopActive = false;
+     }
+    else 
+    {
+        Console.WriteLine("Oops. Try again.");
+     }
+}
+Console.WriteLine("Have a nice day!");
